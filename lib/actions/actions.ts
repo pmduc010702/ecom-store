f9d574ddb88d7eb0b15b9d93c047dbfdc36536c8
pmduc010702 <pmduc010702@gmail.com>
2024-07-17 16:32:10 +0700

    complete full main function

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -22,3 +22,13 @@ export const getCollections = async () => {
     const searchedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/search/${query}`)
     return await searchedProducts.json()
   }
+
+  export const getOrders = async (customerId: string) => {
+    const orders = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`)
+    return await orders.json()
+  }
+  
+  export const getRelatedProducts = async (productId: string) => {
+    const relatedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`)
+    return await relatedProducts.json()
+  }
